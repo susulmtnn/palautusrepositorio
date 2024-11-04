0806044130b68398d79eb3331f70c2c13e407314
Suvi Liimatainen <suvi.liimatainen@gmail.com>
2024-11-04 19:41:30 +0200

    enum works

--- viikko1/nhl-statistics-1/src/index.py
+++ viikko1/nhl-statistics-1/src/index.py
@@ -1,20 +1,28 @@
-from statistics_service import StatisticsService
+from statistics_service import StatisticsService, SortBy
 from player_reader import PlayerReader
 
 
 def main():
     stats = StatisticsService(PlayerReader("https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"))
-    philadelphia_flyers_players = stats.team("PHI")
-    top_scorers = stats.top(10)
+    #philadelphia_flyers_players = stats.team("PHI")
+    #top_scorers = stats.top(10)
 
-    print("Philadelphia Flyers:")
-    for player in philadelphia_flyers_players:
-        print(player)
+    # print("Philadelphia Flyers:")
+    # for player in philadelphia_flyers_players:
+    #     print(player)
 
     print("Top point getters:")
-    for player in top_scorers:
+    for player in stats.top(10, SortBy.POINTS):
         print(player)
 
+
+    print("Top point goal scorers:")
+    for player in stats.top(10, SortBy.GOALS):
+        print(player)
+
+    print("Top by assists:")
+    for player in stats.top(10, SortBy.ASSISTS):
+        print(player)
 
 if __name__ == "__main__":
     main()
--- viikko1/nhl-statistics-1/src/statistics_service.py
+++ viikko1/nhl-statistics-1/src/statistics_service.py
@@ -1,5 +1,10 @@
 from player_reader import PlayerReader
+from enum import Enum
 
+class SortBy(Enum):
+    POINTS=1
+    GOALS=2
+    ASSISTS=3
 
 
 class StatisticsService:
@@ -23,21 +28,16 @@ class StatisticsService:
 
         return list(players_of_team)
 
-    def top(self, how_many):
-        # metodin käyttämä apufufunktio voidaan määritellä näin
-        def sort_by_points(player):
-            return player.points
+    def top(self, how_many, sort_by=SortBy.POINTS):
+        def sort_by_enum(player):
+            if sort_by == SortBy.POINTS:
+                return player.points
+            elif sort_by == SortBy.GOALS:
+                return player.goals
+            elif sort_by == SortBy.ASSISTS:
+                return player.assists
+   
 
-        sorted_players = sorted(
-            self._players,
-            reverse=True,
-            key=sort_by_points
-        )
+        sorted_players = sorted(self._players, key=sort_by_enum, reverse=True)
 
-        result = []
-        i = 0
-        while i < how_many:
-            result.append(sorted_players[i])
-            i += 1
-
-        return result
+        return sorted_players[:how_many]  
