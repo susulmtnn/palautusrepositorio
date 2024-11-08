52eeb6814f7c1b3ade3484ceb3186ec2e644c7d9
Suvi Liimatainen <suvi.liimatainen@gmail.com>
2024-11-08 22:07:36 +0200

    working exc 2

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -12,7 +12,7 @@ class ProjectReader:
         content = request.urlopen(self._url).read().decode("utf-8")
         thisistoml=toml.loads(content)
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        #print(thisistoml)
-        return Project(thisistoml["tool"]["poetry"]["name"], thisistoml["tool"]["poetry"]["description"], thisistoml["tool"]["poetry"]["dependencies"], thisistoml["tool"]["poetry"]["group"]["dev"]["dependencies"])
+        print(thisistoml)
+        return Project(thisistoml["tool"]["poetry"]["name"], thisistoml["tool"]["poetry"]["description"], thisistoml["tool"]["poetry"]["license"], thisistoml["tool"]["poetry"]["authors"], thisistoml["tool"]["poetry"]["dependencies"], thisistoml["tool"]["poetry"]["group"]["dev"]["dependencies"])
 #new=ProjectReader("https://raw.githubusercontent.com/ohjelmistotuotanto-hy/tehtavat/main/viikko2/test-project/pyproject.toml")
 #print(new.get_project())
