551234688aa9c6ec61b1e7041b8ed9ebd338f002
Suvi Liimatainen <suvi.liimatainen@gmail.com>
2024-11-11 18:57:49 +0200

    exercise 10 completed

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,9 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
+        self.team=dict['team']
+        self.goals=dict['goals']
+        self.assists=dict['assists']
     
     def __str__(self):
-        return self.name
+        return self.name + " team " + self.team + " goals " + str(self.goals) + " assists " + str(self.assists)
