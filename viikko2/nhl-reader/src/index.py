0177cd7a45156fabf6fd9150b9b401f276fbd469
Suvi Liimatainen <suvi.liimatainen@gmail.com>
2024-11-11 22:26:48 +0200

    commiting working ex. 12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,35 +1,16 @@
 import requests
 from player import Player
+from player_reader import PlayerReader
+from player_stats import PlayerStats
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-    response = requests.get(url).json()
-
-    print("JSON-muotoinen vastaus:")
-    print(response)
-
-    players = []
-    players_FIN=[]
-
-    for player_dict in response:
-        player = Player(player_dict)
-        players.append(player)
-
-        if player.nationality=='FIN':
-            players_FIN.append({"name": player.name, "team":player.team, "goals": player.goals, "assists": player.assists, "total":player.total})
-        finnish_sorted=sorted(players_FIN, key=lambda player: player["total"], reverse=True)
-
-
-    print("Oliot:")
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
     for player in players:
         print(player)
-
-    print("\nFinnish Players\n")
-
-    for player in finnish_sorted:
-        print(f"{player['name']} {player['team']} {player['goals']} + {player['assists']} = {player['total']}")
-
 
 if __name__ == "__main__":
     main()
