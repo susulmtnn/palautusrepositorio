33e3856808372de06cdce428e704b53bfd2d6970
Suvi Liimatainen <suvi.liimatainen@gmail.com>
2024-11-11 20:05:58 +0200

    ex 10 completed

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -16,7 +16,8 @@ def main():
         players.append(player)
 
         if player.nationality=='FIN':
-            players_FIN.append(player)
+            players_FIN.append({"name": player.name, "team":player.team, "goals": player.goals, "assists": player.assists, "total":player.total})
+        finnish_sorted=sorted(players_FIN, key=lambda player: player["total"], reverse=True)
 
 
     print("Oliot:")
@@ -26,8 +27,8 @@ def main():
 
     print("\nFinnish Players\n")
 
-    for player in players_FIN:
-        print(f"{player.name} {player.team} {player.goals} + {player.assists} = {player.goals + player.assists}")
+    for player in finnish_sorted:
+        print(f"{player['name']} {player['team']} {player['goals']} + {player['assists']} = {player['total']}")
 
 
 if __name__ == "__main__":
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -5,6 +5,7 @@ class Player:
         self.goals=dict['goals']
         self.assists=dict['assists']
         self.nationality=dict['nationality']
+        self.total=self.assists+self.goals
     
     def __str__(self):
         return self.name + " team " + self.team + " goals " + str(self.goals) + " assists " + str(self.assists) + " " + self.nationality
