042631116f3ddaf28026bce81f177ba1b31ef3d5
Suvi Liimatainen <suvi.liimatainen@gmail.com>
2024-12-01 16:42:15 +0200

    commiting some progress

--- viikko5/int-joukko/src/int_joukko.py
+++ viikko5/int-joukko/src/int_joukko.py
@@ -11,14 +11,14 @@ class IntJoukko:
         if kapasiteetti is None:
             self.kapasiteetti = KAPASITEETTI
         elif not isinstance(kapasiteetti, int) or kapasiteetti < 0:
-            raise Exception("Väärä kapasiteetti")  # heitin vaan jotain :D
+            raise Exception("Kapasiteettivirhe: Annettu kapasiteetti joko ei ole luku, tai on negatiivinen")  # heitin vaan jotain :D
         else:
             self.kapasiteetti = kapasiteetti
 
         if kasvatuskoko is None:
             self.kasvatuskoko = OLETUSKASVATUS
         elif not isinstance(kapasiteetti, int) or kapasiteetti < 0:
-            raise Exception("kapasiteetti2")  # heitin vaan jotain :D
+            raise Exception("Kasvatuskokovirhe: Annettu kapasiteetti joko ei ole luku, tai on negatiivinen")  # heitin vaan jotain :D
         else:
             self.kasvatuskoko = kasvatuskoko
 
@@ -26,55 +26,49 @@ class IntJoukko:
 
         self.alkioiden_lkm = 0
 
-    def kuuluu(self, n):
-        on = 0
+    def kuuluu(self, luku):
+        #olisin halunnut muuttaa metodin nimeä, mutta en ollut varma saanko koskea testeihin.
 
-        for i in range(0, self.alkioiden_lkm):
-            if n == self.ljono[i]:
-                on = on + 1
-
-        if on > 0:
+        if luku in self.ljono:
             return True
         else:
             return False
+        
+    def lisaa_eka_luku(self, n):
+            self.ljono[self.alkioiden_lkm]=n
+            self.alkioiden_lkm = self.alkioiden_lkm + 1
+            return True
+    
+    def luo_uusi_sailytyspaikka(self):
+            taulukko_old = self.ljono
+            self.kopioi_lista(self.ljono, taulukko_old)
+            self.ljono = self._luo_lista(self.alkioiden_lkm + self.kasvatuskoko)
+            self.kopioi_lista(taulukko_old, self.ljono)
+            return True
+
 
     def lisaa(self, n):
-        ei_ole = 0
-
         if self.alkioiden_lkm == 0:
-            self.ljono[0] = n
-            self.alkioiden_lkm = self.alkioiden_lkm + 1
-            return True
-        else:
-            pass
-
-        if not self.kuuluu(n):
+            self.lisaa_eka_luku(n)
+        elif not self.kuuluu(n):
             self.ljono[self.alkioiden_lkm] = n
             self.alkioiden_lkm = self.alkioiden_lkm + 1
-
             # ei mahdu enempää, luodaan uusi säilytyspaikka luvuille
             if self.alkioiden_lkm % len(self.ljono) == 0:
-                taulukko_old = self.ljono
-                self.kopioi_lista(self.ljono, taulukko_old)
-                self.ljono = self._luo_lista(self.alkioiden_lkm + self.kasvatuskoko)
-                self.kopioi_lista(taulukko_old, self.ljono)
-
-            return True
-
+                self.luo_uusi_sailytyspaikka()
         return False
 
     def poista(self, n):
-        kohta = -1
         apu = 0
+        indeksi=-1
 
-        for i in range(0, self.alkioiden_lkm):
-            if n == self.ljono[i]:
-                kohta = i  # siis luku löytyy tuosta kohdasta :D
-                self.ljono[kohta] = 0
-                break
+        if n in self.ljono:
+            indeksi=self.ljono.index(n)
+            self.ljono[indeksi]=0
+        
 
-        if kohta != -1:
-            for j in range(kohta, self.alkioiden_lkm - 1):
+        if indeksi != -1:
+            for j in range(indeksi, self.alkioiden_lkm - 1):
                 apu = self.ljono[j]
                 self.ljono[j] = self.ljono[j + 1]
                 self.ljono[j + 1] = apu
